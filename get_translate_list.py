6be171fe4180a15c081ecfdcfabf379642b1cfa4
gomjellie <gomjellie@naver.com>
2017-01-01 12:31:48 +0900

    language코드들 변경, 전에 가져온사이트보다 신뢰할 수 있을거같음

---
+++ get_translate_list.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+#code to get google translate language lists
+url = 'https://ctrlq.org/code/19899-google-translate-languages'
+r = requests.get(url)
+soup = BeautifulSoup(r.text, 'html.parser')
+table = soup.find('tbody')
+
+ret = ""
+
+for tr in table.find_all('tr'):
+    td = tr.find_all('td')
+    ret += "<option value=" + "\"" + td[1].text + "\">" + td[0].text + "</option>"+"\n"
+    ret += "<option value=" + "\"" + td[3].text + "\">" + td[2].text + "</option>"+"\n"
+
+print(ret)
