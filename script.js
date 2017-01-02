eabf8ab505393cea5ee904755b702adb9adcf764
gomjellie <gomjellie@naver.com>
2017-01-02 12:46:54 +0900

    command 처리

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
-// function query(src) {
+// function translatePage(src) {
 //     //컨텐츠 페이지를 대상으로 코드를 실행해주세요.
 //     chrome.tabs.executeScript({
 //         code: "document.querySelector('body').innerText"
 //     }, function(result) {
-
-//         var res = translate(document.querySelector('#src').value);
-
+//         if (result) {
+//             alert(result);
+//             //var res = translate(document.querySelector('#src').value);
+//         }
 //     });
 // }
 
-function query(src) {
+function translatePopup(src) {
     translate(document.querySelector("#src").value);
+}
+
+function isCommands(src) {
+    if (src == 'help' ||
+        src == 'option' ||
+        src == "who made this?" ||
+        src == "reset") {
+        return true;
+    }
+    return false;
+}
+
+function handleCommand(src) {
+    if (src == 'help' || src == 'option') {
+        chrome.tabs.create({ "url": "/options.html", "selected": true }, function(tab) {
+            console.log(tab.id);
+        });
+
+        document.querySelector('#result').innerText = "/help";
+        return true;
+    }
 }
 
 //팝업 페이지의 #src 입력된 값이 변경 되었을때
 if (document.querySelector("#src")) {
     document.querySelector('#src').addEventListener('input', function() {
         var src = document.querySelector('#src').value;
-        query(src);
+        if (isCommands(src)) {
+            handleCommand(src);
+        } else {
+            translatePopup(src);
+        }
     });
 }
 //automatic resize text area using jQuery
