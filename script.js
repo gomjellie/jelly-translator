65225900af4c6b35945434827787d0b7fdb93137
gomjellie <gomjellie@naver.com>
2017-01-05 00:46:36 +0900

    page template가져옴

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ $('p,h1,h2,h3,h4,h5,a').each(function(){
 */
 
 function translatePopup(src) {
-    translate(document.querySelector("#src").value);
+    translate(src);
 }
 
 function isCommands(cmd) {
