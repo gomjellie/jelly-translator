43c564cbb783c2d3db26ea25041a598329bc1424
gomjellie <gomjellie@naver.com>
2017-01-08 00:59:39 +0900

    정리

--- test.js
+++
@@ -1,8 +0,0 @@
-function test_function() {
-    $('a, li, p').each(function() {
-        var text = $(this).text();
-        console.log("text %s is translated", text);
-        $(this).html(text.replace($(this).text(), '번역된 문장'));
-    });
-    alert("test_function_called");
-}
