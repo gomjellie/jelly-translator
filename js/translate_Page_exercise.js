24ae20ea7fc4086a7f57ffb306d19c0e93a29130
gomjellie <gomjellie@naver.com>
2017-01-08 02:40:10 +0900

    page-translate-easy-way

--- js/translate_Page_exercise.js
+++ js/translate_Page_exercise.js
@@ -61,6 +61,7 @@ sM = function(a) {
 //var url = "https://translate.google.com/translate_a/single?client=t&sl=en&tl=ko&hl=ko&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&srcrom=0&ssel=0&tsel=0&kc=1&tk=693132.842370&q=if%20i%20were%20you";
 var base_url = "https://translate.google.com/translate_a/single?client=t&sl=auto&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=0&tsel=0&kc=7&";
 
+var page_url = "https://translate.google.com/translate?sl=auto&tl=ja&js=y&prev=_t&hl=ko&ie=UTF-8&edit-text=&act=url" + "&u=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FMain_Page";
 req = {};
 chrome.storage.local.get(function(data) {
     if (data)
